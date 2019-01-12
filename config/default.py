18aa266d512c8aab49122ebba57fcb3e0e2022a1
nordenskiold <gabriel.nordenskiold@gmail.com>
2019-01-12 14:47:20 -0800

    Add a default config and a function in the __init__ of the flask application to configure config files

---
+++ config/default.py
@@ -0,0 +1,2 @@
+FLICKR_PUBLIC_API_URL = "https://api.flickr.com"
+FLICKR_PUBLIC_PHOTOS = "services/feeds/photos_public.gne?format=json"
--- flickr_photo_wall/__init__.py
+++ flickr_photo_wall/__init__.py
@@ -1,17 +1,26 @@
 from flask import Flask
 
 
-def create_app(config=None):
+def create_app(config="Default"):
     """Create a new flask application"""
     app = Flask(__name__, instance_relative_config=True)
+    configure_cfg(app, config)
     configure_bp(app)
     return app
 
+def configure_cfg(app, config):
+    """Configure flask config files
+
+    Additional conditional config files will be added here as the project grows
+    """
+    app.config.from_object('config.' + config)
 
 def configure_bp(app):
-    """Configure application blueprints"""
-    from flickr_photo_wall.views import search
+    """Configure application blueprints
 
+    Additional blueprints will be added here as the project grows
+    """
+    from flickr_photo_wall.views import search
     app.register_blueprint(search.search_view, url_prefix='/search')
 
 
